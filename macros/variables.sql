d324231ec717f4b451fe23b9d05ab7c78f5b94bd
Venkat Rao <venkatrao.rachamallu@zessta.com>
2024-12-12 15:13:02 +0530

    modified variables and project

--- macros/variables.sql
+++ macros/variables.sql
@@ -1,4 +1,9 @@
 {% macro learn_variables() %}
 
+{% set your_name_jinja = "Venkat Rao" %}
+
+{{ log("Hello " ~ your_name_jinja, info=True) }}
+
+{{ log("Hello " ~ var("user_name", "NO USERNAME IS SET") ~ "!", info=True) }}
 
 {% endmacro %}
